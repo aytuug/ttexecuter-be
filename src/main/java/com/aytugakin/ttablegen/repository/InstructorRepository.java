000ef1e7807b994790696e9871664b665a2e4360
aytugaakin1999@gmail.com <75138903+aytuug@users.noreply.github.com>
2023-09-03 16:29:59 +0300

    test git for github.

--- src/main/java/com/aytugakin/ttablegen/repository/InstructorRepository.java
+++ src/main/java/com/aytugakin/ttablegen/repository/InstructorRepository.java
@@ -13,9 +13,5 @@ public interface InstructorRepository extends JpaRepository<Instructor, Long> {
     Optional<Instructor> findByEmail(String email);
 
     @Query("SELECT DISTINCT (cs.course.id) FROM CourseInstructor cs WHERE cs.course.id = :#{#moduleId}")
-
-
-
-
     List<Long> findInstructorIdsByModuleId(Long moduleId);
 }
